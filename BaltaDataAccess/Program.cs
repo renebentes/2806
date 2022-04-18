6798067f958813d96c2809171c291e8b5f45c2eb
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-04-18 17:40:41 -0300

    build(deps): Adiciona dependência do Microsoft.Data.SqlClient

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -1,3 +1,5 @@
-﻿var connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+﻿using Microsoft.Data.SqlClient;
+
+var connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
