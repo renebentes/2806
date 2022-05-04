ce5206f0278a50d13caa99105583a8b205f5ad76
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-05-04 20:25:33 -0300

    feat: Lê categorias do banco de dados

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 
-using (var connection = new SqlConnection(connectionString))
+using var connection = new SqlConnection(connectionString);
+connection.Open();
+using var command = new SqlCommand();
+command.Connection = connection;
+command.CommandType = CommandType.Text;
+command.CommandText = "SELECT [Id], [Title] FROM Category;";
+
+var reader = command.ExecuteReader();
+
+while (reader.Read())
 {
-    Console.WriteLine("Conectado");
+    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
 }
 
+
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
