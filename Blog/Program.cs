26a2be31d21f5ef25b73b3079103c8c8e7fe09d7
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-08-19 11:44:45 -0300

    feat: Define classa para a conexão com o banco de dados
    Esta classe é estática para ficar disponível para toda a aplicação

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Blog;
+using Microsoft.Data.SqlClient;
 
 const string connectionString = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;";
 
-using var connection = new SqlConnection(connectionString);
+Database.Connection = new SqlConnection(connectionString);
+Database.Connection.Open();
 
 if (!AnsiConsole.Profile.Capabilities.Interactive)
 {
@@ -17,3 +19,5 @@ Thread.Sleep(2000);
 Clear();
 
 MainScreen.Load();
+
+Database.Connection.Close();
